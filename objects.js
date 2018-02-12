aa22a8f19d00921876271e256d2f6db0d284f399
Jorge Ramirez <jorgeramirez927@gmail.com>
2018-02-12 17:50:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,10 @@
 var playlist = {drake:"Look Alive"}
 
 function updatePlaylist(playlist, artist,song){
-  playlist[artist]=song
+  playlist[artist]= song
   return playlist
+}
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist]
 }
